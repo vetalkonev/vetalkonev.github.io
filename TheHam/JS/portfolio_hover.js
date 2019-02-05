eb4020b809987c93b74eb16288be0a67b3c4d8e1
Вит Кон <vetalkonev13@gmail.com>
2019-02-05 11:53:04 +0200

    Final stage

--- TheHam/JS/portfolio_hover.js
+++ TheHam/JS/portfolio_hover.js
@@ -2,8 +2,7 @@ $(".element-item").hover(function() {
 
     let portfolioInfo = $(this).attr("data-info");
     portfolioInfo = portfolioInfo.split("; ");
-    $(this).prepend(`<div class="portfolio-item-info">
-          <div class="portfolio-info-icons">
+    $(this).prepend(`<div class="portfolio-item-info"><div class="portfolio-info-icons">
               <a href="#" class="portfolio-info-icon"><i class="fas fa-link"></i></a>
               <a href="#" class="portfolio-info-icon"><i class="fas fa-search"></i></a>
           </div>
