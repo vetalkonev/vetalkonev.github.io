ab73b3d1e9867f49f49af55a15c539709dc3f036
Вит Кон <vetalkonev13@gmail.com>
2019-02-07 21:56:46 +0200

    commit

--- TheHam/JS/portfolio_hover.js
+++ TheHam/JS/portfolio_hover.js
@@ -1,15 +1,15 @@
-$(".element-item").hover(function() {
+$(".element-item").hover(function () {
 
     let portfolioInfo = $(this).attr("data-info");
     portfolioInfo = portfolioInfo.split("; ");
     $(this).prepend(`<div class="portfolio-item-info"><div class="portfolio-info-icons">
-              <a href="#" class="portfolio-info-icon"><i class="fas fa-link"></i></a>
+                <a href="#" class="portfolio-info-icon"><i class="fas fa-link"></i></a>
               <a href="#" class="portfolio-info-icon"><i class="fas fa-search"></i></a>
           </div>
           <h3 class="portfolio-info-title">${portfolioInfo[0]}</h3>
           <p class="portfolio-info-description">${portfolioInfo[1]}</p>
       </div>`);
 
-}, function() {
+}, function () {
     $(this).find(".portfolio-item-info").remove();
 });
